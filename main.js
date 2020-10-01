65a792dc09360068a7131222c60379bcdefca562
Vanessa Covarrubias <acovarrubias10@ucol.mx>
2020-09-30 20:29:16 -0500

    Subiendo script

---
+++ main.js
@@ -0,0 +1,28 @@
+class Main {
+    testSueldo() {
+        let sueldo = 1600
+        if (sueldo <= 1000){
+            let nuevoSueldo =(sueldo*1.15)
+            console.log("Sueldo " + nuevoSueldo)
+        }
+        else if (sueldo <= 1200){
+            let nuevoSueldo =(sueldo*1.12)
+            console.log("Sueldo " + nuevoSueldo)
+        }
+        else if (sueldo <= 1400){
+            let nuevoSueldo =(sueldo*1.1)
+            console.log("Sueldo " + nuevoSueldo)
+        }
+        else if (sueldo <= 1500){
+            let nuevoSueldo =(sueldo*1.08)
+            console.log("Sueldo " + nuevoSueldo)
+        }
+        else if (sueldo > 1500){
+            let nuevoSueldo =(sueldo*1.08)
+            console.log("Sueldo " + nuevoSueldo)
+        }
+    }
+}
+let app = new Main () 
+
+app.testSueldo()
